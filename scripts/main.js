8d00cf09550f37ea295ff7abeb3ff71010f5809d
Callum <Callum@Andreass-MacBook-Pro.local>
2018-08-31 15:17:55 +0200

    Ranking Table Update
    additional minor changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,23 +3,29 @@ var readmoreBtn = document.querySelectorAll('.ranking-table .partner-row .inform
 readmoreBtn.forEach(function(el, i) {
 
   var parentRow = el.parentElement.parentElement;
+
+  // Set variable for row height
   var height = parentRow.scrollHeight;
 
-  var information = parentRow.querySelector('.information')
 
   el.addEventListener('click', function(){
 
 
     var content = parentRow.querySelector('.info');
 
+    // Toggle '--expand' class on row element
     parentRow.classList.toggle('--expand')
 
     if (content.style.maxHeight) {
       content.style.maxHeight = null
       parentRow.style.height = height + "px"
+
+      el.textContent = "Read More"
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
       parentRow.style.height = height + content.scrollHeight + "px"
+
+      el.textContent = "Read Less"
     }
 
   })
