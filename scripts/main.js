38ff882cb981b8bc2e2c3496cb14d7864d597b02
Callum <Callum@Andreass-MacBook-Pro.local>
2018-09-03 15:24:24 +0200

    Ranking Table updates
     - Logic adjustments for row expanding
    - Long text in row information fixes
    - Firefox ellipsis fixes

--- scripts/main.js
+++ scripts/main.js
@@ -12,36 +12,37 @@ readmoreBtn.forEach(function(el, i) {
 
   el.addEventListener('click', function(){
 
+    // Calculate height of Partner information elements before expanding
+    var preX = parentRow.querySelector('.information .name').offsetHeight
+    var preY = parentRow.querySelector('.information .membercount').offsetHeight
+    var preZ = parentRow.querySelector('.information .tag').offsetHeight
 
-    var preX = parentRow.querySelector('.information .name').scrollHeight
-    var preY = parentRow.querySelector('.information .membercount').scrollHeight
-    var preZ = parentRow.querySelector('.information .tag').scrollHeight
-
+    // Height of bullet point list
     var content = parentRow.querySelector('.info');
-
-    // console.log(height);
-    // console.log(content.scrollHeight);
 
     // Toggle '--expand' class on row element
     parentRow.classList.toggle('--expand')
 
-    // ============================================================
-    // ============================================================
-
+    // Calculate height of Partner information elements after expanding (there may be discrepancies with line breaking)
     var x = parentRow.querySelector('.information .name').scrollHeight
     var y = parentRow.querySelector('.information .membercount').scrollHeight
     var z = parentRow.querySelector('.information .tag').scrollHeight
 
+    // Subtract pre height, and current height elements from each other to get true height value
     var height = (x - preX) + (y - preY) + (z - preZ);
 
     // var newX = rowHeight - height;
 
-    // console.log("name = " + x)
-    // console.log("membercount = " + y)
-    // console.log("tag = " + z)
+    console.log("name = " + x)
+    console.log("pre name = " + preX)
+    console.log("membercount = " + y)
+    console.log("pre membercount = " + preY)
+    console.log("pre tag = " + preZ)
+    console.log("tag = " + z)
 
-    console.log("info height = " + height);
-    // console.log("row height = " + rowHeight);
+    console.log("information height = " + height);
+    console.log("row height = " + rowHeight);
+    console.log("content height = " + content.scrollHeight);
 
     // ============================================================
     // ============================================================
