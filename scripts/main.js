f03885d04be4c7f163062d4df9f2625012197c74
Callum <Callum@Andreass-MacBook-Pro.local>
2018-09-03 13:22:25 +0200

    Ranking Table Updates
    - Added Banner colours
    - Hover styles added for buttons and links
    - Firefox linebreak ellipsis fix
    - Js logic adjustment for row height on expand

--- scripts/main.js
+++ scripts/main.js
@@ -5,25 +5,55 @@ readmoreBtn.forEach(function(el, i) {
   var parentRow = el.parentElement.parentElement;
 
   // Set variable for row height
-  var height = parentRow.scrollHeight;
+  var rowHeight = parentRow.scrollHeight;
+
+
 
 
   el.addEventListener('click', function(){
 
 
+    var preX = parentRow.querySelector('.information .name').scrollHeight
+    var preY = parentRow.querySelector('.information .membercount').scrollHeight
+    var preZ = parentRow.querySelector('.information .tag').scrollHeight
+
     var content = parentRow.querySelector('.info');
+
+    // console.log(height);
+    // console.log(content.scrollHeight);
 
     // Toggle '--expand' class on row element
     parentRow.classList.toggle('--expand')
 
+    // ============================================================
+    // ============================================================
+
+    var x = parentRow.querySelector('.information .name').scrollHeight
+    var y = parentRow.querySelector('.information .membercount').scrollHeight
+    var z = parentRow.querySelector('.information .tag').scrollHeight
+
+    var height = (x - preX) + (y - preY) + (z - preZ);
+
+    // var newX = rowHeight - height;
+
+    // console.log("name = " + x)
+    // console.log("membercount = " + y)
+    // console.log("tag = " + z)
+
+    console.log("info height = " + height);
+    // console.log("row height = " + rowHeight);
+
+    // ============================================================
+    // ============================================================
+
     if (content.style.maxHeight) {
       content.style.maxHeight = null
-      parentRow.style.height = height + "px"
+      parentRow.style.height = rowHeight + "px"
 
       el.textContent = "Read More"
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-      parentRow.style.height = height + content.scrollHeight + "px"
+      parentRow.style.height = rowHeight + height + content.scrollHeight + "px"
 
       el.textContent = "Read Less"
     }
